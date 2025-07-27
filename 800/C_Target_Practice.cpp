b2887a6670b6d43470ef0d8e682fe8f19a97469f
thelocalminima <p.akhilraja@gmail.com>
2025-07-27 23:38:31 +0000

    cp-31-800-1

---
+++ 800/C_Target_Practice.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+
+void solve() {
+    vector<vector<char>> grid(10,vector<char>(10,'.'));
+    int score = 0;
+
+    for(int i=0;i<10;i++){
+        for(int j=0;j<10;j++){
+            cin>>grid[i][j];
+        }
+    }
+
+    
+    for(int i=0;i<10;i++){
+        for(int j=0;j<10;j++){
+            if(i == 0 || i == 9 || j == 0 || j == 9){
+                if(grid[i][j]=='X')
+                    score++;
+            }
+            else if(i == 1 || i == 8 || j == 1 || j == 8){
+                if(grid[i][j]=='X')
+                    score+=2;
+            }
+            else if(i == 2 || i == 7 || j == 2 || j == 7){
+                if(grid[i][j]=='X')
+                    score+=3;
+            }
+            else if(i == 3 || i == 6 || j == 3 || j == 6){
+                if(grid[i][j]=='X')
+                    score+=4;
+            }
+            else if(i == 4 || i == 5 || j == 4 || j == 5){
+                if(grid[i][j]=='X')
+                    score+=5;
+            }
+        }
+    }
+    cout<<score<<endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
